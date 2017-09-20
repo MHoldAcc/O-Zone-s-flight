c9efc438fe2a10674bcfd8738cbab8250ed748dc
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-09-20 10:56:45 +0200

    Created Player

--- O-ZonesFlight/app/src/main/java/onebit/o_zonesflight/Player.java
+++ O-ZonesFlight/app/src/main/java/onebit/o_zonesflight/Player.java
@@ -4,8 +4,19 @@ package onebit.o_zonesflight;
  * Created by admin on 20.09.2017.
  */
 public class Player {
+    //Settings for development
+    private float setting_deathZone = (float)0.1;
+    private float setting_maxPosition = 100;
+    private float setting_minPosition = 0;
+    private float setting_maxMovement = 5;
+
     //0-100
     private float Position;
+
+    public Player(){
+        int position = (int) ((setting_minPosition + setting_maxPosition) / 2);
+        Position = (long)position;
+    }
 
     public float GetPosition(){
         return Position;
@@ -13,6 +24,20 @@ public class Player {
 
     //bearing = -1 - 1
     public void ApplyBearing(float bearing){
-        
+        //Applies Deathzone 0.1
+        if(bearing >= -setting_deathZone && bearing <= setting_deathZone)
+            bearing = 0;
+        //Rounds if to big number
+        if(bearing > 1)
+            bearing = 1;
+        else if(bearing < -1)
+            bearing = -1;
+        //Sets position
+        float movement = setting_maxMovement * bearing;
+        Position = Position + movement;
+        if(Position > setting_maxPosition)
+            Position = setting_maxPosition;
+        else if (Position < setting_minPosition)
+            Position = setting_minPosition;
     }
 }
