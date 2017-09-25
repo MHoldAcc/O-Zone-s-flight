1e313103cf9f03ce757f01c1448bdd1f2eb156f1
Silvan Pfister <silvan.pfister@hotmail.com>
2017-09-25 16:03:33 +0200

    Commented GameTickRunnable and Settings in detail

--- O-ZonesFlight/app/src/main/java/onebit/o_zonesflight/GameTickRunnable.java
+++ O-ZonesFlight/app/src/main/java/onebit/o_zonesflight/GameTickRunnable.java
@@ -1,23 +1,24 @@
 package onebit.o_zonesflight;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.PointF;
-import android.widget.ImageView;
-
 /**
- * Created by admin on 20.09.2017.
+ * Class is a Runnable that simply calls the Gametick on the uicontroller.
+ * Created by silvan pfister on 20.09.2017.
  */
 public class GameTickRunnable implements Runnable {
 
     private final UIController owner;
 
+    /**
+     * Creates a GameTickRunnable instance.
+     * @param owner The UIController which GameTick method should be ran
+     */
     public GameTickRunnable(UIController owner){
         this.owner = owner;
     }
 
+    /**
+     * Simply runs the GameTick method of the UIController associated with this instance
+     */
     @Override
     public void run() { owner.GameTick(); }
 
--- O-ZonesFlight/app/src/main/java/onebit/o_zonesflight/Settings.java
+++ O-ZonesFlight/app/src/main/java/onebit/o_zonesflight/Settings.java
@@ -7,23 +7,38 @@ package onebit.o_zonesflight;
  */
 public class Settings {
     //***Gameplay***
+    /** The amount of milliseconds until a new meteorite is spawned */
     protected final static int Gameplay_TimeTillNewMeteorite = 1000;
+    /** The amount of milliseconds until a velocity increase takes place */
     protected final static int Gameplay_TimeTillVelocityIncrease = 5000;
+    /** The velocity multiplier for a velocity increase */
     protected final static float Gameplay_VelocityIncrease = 1.1f;
+    /** The amount of milliseconds each frame should have */
     protected final static int Gameplay_MillisecondsPerFrame = 33;
     //***Player***
+    /** The maximum amount of units the player may move in a single tick */
     protected final static int Player_MaxMovement = 40;
+    /** The height of the player in units */
     protected final static int Player_Height = 10;
+    /** The width of the player in units */
     protected final static int Player_Width = 20;
     //***Environment Sizes***
+    /** The width of the game world in units */
     protected final static int Environment_Width = 100;
+    /** The height of the game world in units */
     protected final static int Environment_Height = 100;
+    /** The amount of paths a meteorite may fall on */
     protected final static int Environment_LineCount = 3;
+    /** The width of a path a meteorite takes, calculated from the Environment_Width and the Environment_LineCount */
     protected final static int Environment_LineWidth = Environment_Width / Environment_LineCount;
     //***Meteorites***
+    /** The base speed of the meteorites in Unites per Tick*/
     protected final static int Meteorites_Movement = 3;
+    /** The height of a meteorite, matching the Environment_LineWidth */
     protected final static int Meteorites_Height = Environment_LineWidth;
     //***Inputs***
+    /** The DeathZone, the threshold of input that is ignored from the hardware sensor */
     protected final static float Inputs_DeathZone = 0.02f;
+    /** The upper limit of the input that is accepted */
     protected final static float Inputs_MaxInput = 0.3f;
 }
