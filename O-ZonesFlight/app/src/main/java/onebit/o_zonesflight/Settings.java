5f3357226538132b7d4e544ef217a1fe46fa16f0
Silvan Pfister <silvan.pfister@hotmail.com>
2017-09-25 09:29:05 +0200

    Applied minor changes to settings. Fixed rendering issue in UIController.

--- O-ZonesFlight/app/src/main/java/onebit/o_zonesflight/Settings.java
+++ O-ZonesFlight/app/src/main/java/onebit/o_zonesflight/Settings.java
@@ -5,23 +5,21 @@ package onebit.o_zonesflight;
  */
 public class Settings {
     //***Gameplay***
-    protected static int Gameplay_TimeTillNewMeteorite = 5000;
-    protected static int Gameplay_TimeTillVelocityIncrease = 10000;
-    protected static float Gameplay_VelocityIncrease = (float)1.1;
-    protected static int Gameplay_MillisecondsPerFrame = 33;
+    protected final static int Gameplay_TimeTillNewMeteorite = 5000;
+    protected final static int Gameplay_TimeTillVelocityIncrease = 10000;
+    protected final static float Gameplay_VelocityIncrease = 1.1f;
+    protected final static int Gameplay_MillisecondsPerFrame = 33;
     //***Player***
-    protected static int Player_MaxMovement = 5;
-    protected static int Player_Height = 10;
-    protected static int Player_Width = 20;
+    protected final static int Player_MaxMovement = 5;
+    protected final static int Player_Height = 10;
+    protected final static int Player_Width = 20;
     //***Meteorites***
-    protected static int Meteorites_Movement = 1;
+    protected final static int Meteorites_Movement = 1;
     //***Environment Sizes***
-    protected static int Environment_Width = 100;
-    protected static int Environment_Height = 100;
-    protected static int Environment_LineCount = 3;
-    protected static int Environment_LineWidth(){
-        return (int)Environment_Width / 3;
-    }
+    protected final static int Environment_Width = 100;
+    protected final static int Environment_Height = 100;
+    protected final static int Environment_LineCount = 3;
+    protected final static int Environment_LineWidth = Environment_Width / Environment_LineCount;
     //***Inputs***
-    protected static float Inputs_DeathZone = (float)0.1;
+    protected final static float Inputs_DeathZone = 0.1f;
 }
